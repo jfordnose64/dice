04d7d7e6daef199334ae269ea84e61e102d99ffb
jackson ford <jfordnose64@gmail.com>
2019-08-06 09:53:31 -0400

    added js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let diceRoller = document.querySelector('.dice-roller')
+let numberRolled = document.querySelector('.roll-number')
+const rollButton = document.querySelector('.roll-button')
+// const twentySided = document.querySelector('.20-sided')
+// let twelveSided = document.querySelector('.12-sided')
+// let tenSided = document.querySelector('.10-sided')
+// let eightSided = document.querySelector('.8-sided')
+// let fourSided = document.querySelector('.4-sided')
+// let hundredSided = document.querySelector('.100-sided')
+
+diceRoller = () => {
+  const randomNum = Math.floor(Math.random() * 8) + 1
+  console.log(randomNum)
+  document.querySelector('.roll-number').textContent = randomNum
 }
 
-document.addEventListener('DOMContentLoaded', main)
+// const diceRollerTwenty = () => {
+//   const randomNum = Math.floor(Math.random() * 20) + 1
+//   document.querySelector('.roll-number').textContent = randomNum
+// }
+rollButton.addEventListener('click', diceRoller)
+
+// twentySided.addEventListener('click', diceRollerTwenty)
